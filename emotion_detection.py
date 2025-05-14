f5e828d5800fb067fe709f9ae869fe475d0b7355
Nishala-M <nnishala3@gmail.com>
2025-05-14 19:13:21 +0530

    emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,17 +10,8 @@ Original file is located at
 !wget https://github.com/oarriaga/face_classification/raw/master/trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5 -O emotion_model.h5
 
 
-
-
-
-
-
 from google.colab import files
 uploaded = files.upload()
-
-
-
-
 
 
 
